fa4a131fbd98cad4d4dccd5548ce88a8911c8ae5
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-05-28 07:19:42 +0530

    main modified

--- src/main/java/com/near/places/Main.java
+++ src/main/java/com/near/places/Main.java
@@ -3,11 +3,9 @@ package com.near.places;
 import com.near.places.service.OperatorInfo;
 import com.near.places.service.PoiInfo;
 import com.near.places.utility.Resource;
-import java.io.IOException;
-
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     Resource resource=new Resource();
     OperatorInfo operatorInfo=new OperatorInfo();
     PoiInfo poiInfo=new PoiInfo();
@@ -17,7 +15,7 @@ public class Main {
     // Mobile devices used under different operators
     operatorInfo.mobileInfoForOperators();
 
-    // Nearest POI’s for all the lat lons in
+    // Nearest POI’s for all the latitude longitude in
     poiInfo.nearestPoiDetail();
 
   }
