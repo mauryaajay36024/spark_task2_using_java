5555412a758295654bd625815361b2e3acdf60e9
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-05-28 17:53:27 +0530

    PoiInfo modified

--- src/main/java/com/near/places/service/PoiInfo.java
+++ src/main/java/com/near/places/service/PoiInfo.java
@@ -8,9 +8,18 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PoiInfo implements Serializable {
   // Resource creates SparkSession
@@ -28,6 +37,7 @@ public class PoiInfo implements Serializable {
 
         // fetch the details for nearest poiId
         getDataFromPoiId(poiId);
+
       }
     });
   }
@@ -35,7 +45,7 @@ public class PoiInfo implements Serializable {
   public String getPoiId(double latitude,double longitude) throws IOException {
     //Sending GET request to fetch PoiId
     HttpClient client = new DefaultHttpClient();
-    HttpGet request = new HttpGet(String.format("http://places.zprk.io/v1/proxim/%f/%f/1000?withdist=true&sort=ASC",latitude,longitude));
+    HttpGet request = new HttpGet(String.format(resource.properties.getProperty("PROXIMAV1API"),latitude,longitude));
     HttpResponse response = client.execute(request);
 
     //get jsonNode out of http response
@@ -46,15 +56,37 @@ public class PoiInfo implements Serializable {
 
   public void getDataFromPoiId(String poiId) throws IOException {
     HttpClient client = new DefaultHttpClient();
-    HttpGet request = new HttpGet(String.format("http://places.zprk.io/v2/places/IND?key=nearplacestestkey&near_poi_id=%s", poiId));
+    HttpGet request = new HttpGet(String.format(resource.properties.getProperty("PROXIMAV2API"), poiId));
     HttpResponse response = client.execute(request);
 
     //get jsonNode out of http response
     JsonNode jsonNode = stringToJson(response);
+
     String brand = jsonNode.get("payload").get(0).get("name").asText();
     String category1 = jsonNode.get("payload").get(0).get("cat1").asText();
     String category2 = jsonNode.get("payload").get(0).get("cat2").asText();
-    System.out.printf("brand=%s category1=%-25s category2=%-25s%n", brand, category1, category2);
+
+    List<String> list=new ArrayList<>();
+    list.add(brand);
+    list.add(category1);
+    list.add(category2);
+
+    //Converting dataframe for HTTP response
+    JavaSparkContext sparkContext = new JavaSparkContext();
+    JavaRDD<Row> rowRDD = sparkContext.parallelize(list).map((String row) -> RowFactory.create(row));
+    StructType schema = DataTypes.createStructType(
+        new StructField[] {
+            DataTypes.createStructField("brand", DataTypes.StringType, false),
+            DataTypes.createStructField("category1", DataTypes.StringType, false),
+            DataTypes.createStructField("category2", DataTypes.StringType, false)
+        });
+
+    Dataset<Row> df=resource.getSpark().sqlContext().createDataFrame(rowRDD,schema).toDF();
+
+    df.write()
+        .mode("overwrite")
+        .format("csv")
+        .save(resource.properties.getProperty("POIINFOPATH"));
   }
 
   //To convert the http response to json object
