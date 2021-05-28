6f5a2160f0a5ef4dc17d766471232b03b4934837
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-05-28 07:21:55 +0530

    main modified

--- src/main/java/com/near/places/service/OperatorInfo.java
+++ src/main/java/com/near/places/service/OperatorInfo.java
@@ -5,28 +5,27 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 public class OperatorInfo {
+  static final String QUERY="select devCarrier,count(distinct ifa) AS numberOfMobileDevices from placesData group by devCarrier order by numberOfMobileDevices desc";
   Resource resource=new Resource();
-  public void mobileInfoForOperators(){
 
-    Dataset dataFrame = resource.getDataFrame();
+  public void mobileInfoForOperators(){
+    Dataset<Row> dataFrame = resource.getDataFrame();
 
     long noOfOperators=dataFrame.select("devCarrier").distinct().count();
-    System.out.println("Number of Operators :"+noOfOperators);
+    System.out.printf("No of Operators:: %o%n",noOfOperators);
 
     // Register the DataFrame as a SQL temporary view
     dataFrame.createOrReplaceTempView("placesData");
 
     // number of mobile devices used under different operators
-    Dataset<Row> data=resource.getSpark().sql("select devCarrier,count(distinct ifa) AS numberOfMobileDevices from placesData group by devCarrier order by numberOfMobileDevices desc");
-
-    data.show();
+    Dataset<Row> data=resource.getSpark().sql(QUERY);
 
     data.write()
         .option("header","true")
         .option("sep",",")
         .mode("overwrite")
         .format("csv")
-        .save("/Users/ajaymaurya/Downloads/spark/sparkTaskJava1/");
+        .save(resource.properties.getProperty("OPERATORFILEPATH"));
 
     System.out.println("Data Saved successfully");
   }
