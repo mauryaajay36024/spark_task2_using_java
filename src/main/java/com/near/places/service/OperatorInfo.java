660f1bff7a0c297f1777f70017ccbc5666a8ed16
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-01 14:59:52 +0530

    string changed to string constant

--- src/main/java/com/near/places/service/OperatorInfo.java
+++ src/main/java/com/near/places/service/OperatorInfo.java
@@ -1,31 +1,29 @@
 package com.near.places.service;
 
 import com.near.places.utility.Resource;
+import com.near.places.utility.StringConst;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 public class OperatorInfo {
-  static final String QUERY="select devCarrier,count(distinct ifa) AS numberOfMobileDevices from placesData group by devCarrier order by numberOfMobileDevices desc";
   Resource resource=new Resource();
 
   public void mobileInfoForOperators(){
     Dataset<Row> dataFrame = resource.getDataFrame();
 
-    long noOfOperators=dataFrame.select("devCarrier").distinct().count();
+    long noOfOperators=dataFrame.select(StringConst.DEV_CARRIER).distinct().count();
     System.out.printf("No of Operators:: %o",noOfOperators);
 
     // Register the DataFrame as a SQL temporary view
-    dataFrame.createOrReplaceTempView("placesData");
+    dataFrame.createOrReplaceTempView(StringConst.PLACES_DATA);
 
     // number of mobile devices used under different operators
-    Dataset<Row> data=resource.getSpark().sql(QUERY);
-
+    Dataset<Row> data=resource.getSpark().sql(StringConst.QUERY);
     data.write()
-        .option("header","true")
-        .option("sep",",")
-        .mode("overwrite")
-        .format("csv")
-        .save(resource.properties.getProperty("OPERATORFILEPATH"));
-    System.out.println("Data Saved successfully");
+        .option(StringConst.HEADER,StringConst.TRUE)
+        .option(StringConst.SEP,",")
+        .mode(StringConst.OVERWRITE)
+        .format(StringConst.PARQUET)
+        .save(resource.properties.getProperty(StringConst.OPERATORFILEPATH));
   }
 }
