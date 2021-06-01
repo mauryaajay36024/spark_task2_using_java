fdc80b3b72506cc85572fb279f2f8182a158b9fe
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-01 15:03:57 +0530

    PoiInfo.java deleted

--- src/main/java/com/near/places/service/PoiInfo.java
+++ src/main/java/com/near/places/service/PoiInfo.java
@@ -3,6 +3,7 @@ package com.near.places.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.near.places.utility.Resource;
+import com.near.places.utility.StringConst;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -14,53 +15,61 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class PoiInfo implements Serializable {
   Resource resource=new Resource();
   static List<String> poiDetailList=new ArrayList<>();
+  static List<String> poiIdList = new ArrayList<>();
   public void nearestPoiDetail() {
     Dataset<Row> dataFrame=resource.getDataFrame();
-    Dataset<Row> locationDataRdd = dataFrame.select("lat","lng");
+    Dataset<Row> locationDataRdd = dataFrame.select(StringConst.LAT,StringConst.LON);
 
-    JavaRDD<Row> locationData = locationDataRdd.toJavaRDD();
+    // Fetch nearest poiId for given latitude and longitude
+    JavaRDD<Row> locationData = locationDataRdd.limit(50).toJavaRDD();
     locationData.foreachPartition(iterator -> {
       while (iterator.hasNext()) {
         Row row = iterator.next();
-        String poiId=null;
-        // Fetch nearest poiId for given latitude and longitude
+        String poiId;
         if(!row.anyNull()) {
           poiId = getPoiId(row.getDouble(0), row.getDouble(1));
-        }
-
-         //fetch the details for nearest poiId
-        if(poiId !=null) {
-          getDataFromPoiId(poiId);
+          if(poiId !=null) {
+            poiIdList.add(poiId);
+          }
         }
       }
     });
+    //Converting poiIdList into dataframe
+    Dataset<Row> poiIdDataset = resource.getSpark().createDataset(poiIdList, Encoders.STRING()).toDF();
+    Dataset<Row> poiData = poiIdDataset.select(StringConst.POI_iD);
 
+    // Fetch nearest poiId details
+    poiData.foreachPartition(iterator -> {
+      while (iterator.hasNext()) {
+        Row row = iterator.next();
+        if(!row.anyNull()) {
+          getDataFromPoiId(row.getString(0));
+        }
+      }
+    });
     savePoiDetailToFile(poiDetailList);
   }
 
   public String getPoiId(double latitude,double longitude)  {
     //Sending GET request to fetch PoiId
     HttpClient client = new DefaultHttpClient();
-    HttpGet request = new HttpGet(String.format(resource.properties.getProperty("PROXIMAV1API"),latitude,longitude));
+    HttpGet request = new HttpGet(String.format(resource.properties.getProperty(StringConst.PROXIMAV1API),latitude,longitude));
     HttpResponse response = null;
     try {
       response = client.execute(request);
     } catch (IOException e) {
       e.printStackTrace();
     }
-
     //get jsonNode out of http response
     JsonNode jsonNode = null;
     if(response !=null) {
       jsonNode = convertStringToJson(response);
     }
-
-    if(jsonNode !=null && jsonNode.get("places").has(0)) {
-      String[] poiId = jsonNode.get("places").get(0).get(0).asText().split("\\|");
+    if(jsonNode !=null && jsonNode.get(StringConst.PLACES).has(0)) {
+      String[] poiId = jsonNode.get(StringConst.PLACES).get(0).get(0).asText().split("\\|");
       return poiId[0];
     }
     return null;
@@ -68,7 +77,7 @@ public class PoiInfo implements Serializable {
 
   public void getDataFromPoiId(String poiId)  {
     HttpClient client = new DefaultHttpClient();
-    HttpGet request = new HttpGet(String.format(resource.properties.getProperty("PROXIMAV2API"), poiId));
+    HttpGet request = new HttpGet(String.format(resource.properties.getProperty(StringConst.PROXIMAV2API), poiId));
     HttpResponse response = null;
     try {
       response = client.execute(request);
@@ -80,43 +89,45 @@ public class PoiInfo implements Serializable {
     if(response !=null) {
       jsonNode = convertStringToJson(response);
     }
-
-    if(jsonNode !=null && jsonNode.has("payload") && jsonNode.get("payload").get(0).has("name")
-    && jsonNode.get("payload").get(0).has("cat1") && jsonNode.get("payload").get(0).has("cat2")) {
-      String brand = jsonNode.get("payload").get(0).get("name").asText();
-      String category1 = jsonNode.get("payload").get(0).get("cat1").asText();
-      String category2 = jsonNode.get("payload").get(0).get("cat2").asText();
-      String data=String.format("%s,%s,%s",brand,category1,category2);
-      poiDetailList.add(data);
+    if(jsonNode !=null) {
+      if(jsonNode.get(StringConst.PAYLOAD).get(0) !=null ) {
+        if(jsonNode.get(StringConst.PAYLOAD).get(0).has(StringConst.NAME)
+            && jsonNode.get(StringConst.PAYLOAD).get(0).has(StringConst.CAT1) && jsonNode.get(StringConst.PAYLOAD).get(0).has(StringConst.CAT2)) {
+          String brand = jsonNode.get(StringConst.PAYLOAD).get(0).get(StringConst.NAME).asText();
+          String category1 = jsonNode.get(StringConst.PAYLOAD).get(0).get(StringConst.CAT1).asText();
+          String category2 = jsonNode.get(StringConst.PAYLOAD).get(0).get(StringConst.CAT2).asText();
+          String nearestPoiData = String.format("%s,%s,%s", brand, category1, category2);
+          poiDetailList.add(nearestPoiData);
+        }
+      }
     }
   }
 
   //To convert the http response to json object
   public JsonNode convertStringToJson(HttpResponse response)  {
-    String responseData = null;
-    try {
-      responseData = EntityUtils.toString(response.getEntity());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    ObjectMapper objectMapper = new ObjectMapper();
-    try {
-      return objectMapper.readTree(responseData);
-    } catch (IOException e) {
-      e.printStackTrace();
+    if(response.getStatusLine().getStatusCode()==200) {
+      String responseData = null;
+      try {
+        responseData = EntityUtils.toString(response.getEntity());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      ObjectMapper objectMapper = new ObjectMapper();
+      try {
+        return objectMapper.readTree(responseData);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
     return null;
   }
 
   public void savePoiDetailToFile(List<String> poiDetailList){
-    //Converting into dataframe for HTTP response
-    Dataset<String> poiDataset = resource.getSpark().createDataset(poiDetailList,Encoders.STRING());
-    Dataset<Row> poiDetailDataframe = resource.getSpark().read().json(poiDataset);
-
-    poiDetailDataframe.write()
-        .mode("overwrite")
-        .format("parquet")
-        .save(resource.properties.getProperty("POIINFOPATH"));
-
+    //Converting poiDetailList into dataframe
+    Dataset<Row> poiDataset = resource.getSpark().createDataset(poiDetailList,Encoders.STRING()).toDF();
+    poiDataset.write()
+        .mode(StringConst.OVERWRITE)
+        .format(StringConst.PARQUET)
+        .save(resource.properties.getProperty(StringConst.POIINFOPATH));
   }
 }
